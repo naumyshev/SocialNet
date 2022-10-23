7207a4b4560c4cca9a086a93167f77d4b23e99af
Aleksei Naumyshev <naumyshev@yandex.ru>
2022-10-23 21:19:08 +0300

    #11 completed: grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,56 @@
 import React from 'react';
 import './App.css';
+import logo from './pic/logo512.png'
+import lake from './pic/canada-in-pictures-beautiful-places-to-photograph-morraine-lake.jpg'
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div className="app-wrapper">
+            <header className='header'>
+                <img src={logo} alt="logo"/>
+            </header>
+            <nav className='nav'>
+                <div>
+                    <a href="">Profile</a>
+                </div>
+                <div>
+                    <a href="">Messages</a>
+                </div>
+                <div>
+                    <a href="">News</a>
+                </div>
+                <div>
+                    <a href="">Music</a>
+                </div>
+                <div>
+                    <a href="">Settings</a>
+                </div>
+            </nav>
+            <div className='content'>
+                <div>
+                    <img src={lake} alt=""/>
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
