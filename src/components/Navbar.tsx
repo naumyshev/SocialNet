317c413d02e37e97fb33001758827dc18bf7b46f
Aleksei Naumyshev <naumyshev@yandex.ru>
2022-10-25 14:20:31 +0300

    #14 completed: added css for components

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,19 +3,19 @@ import React from 'react';
 export const Navbar = () => {
     return (
         <nav className='nav'>
-            <div>
+            <div className='nav'>
                 <a href="">Profile</a>
             </div>
-            <div>
+            <div className='nav'>
                 <a href="">Messages</a>
             </div>
-            <div>
+            <div className='nav'>
                 <a href="">News</a>
             </div>
-            <div>
+            <div className='nav'>
                 <a href="">Music</a>
             </div>
-            <div>
+            <div className='nav'>
                 <a href="">Settings</a>
             </div>
         </nav>
