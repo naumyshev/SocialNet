e032fe1e30bbd26827c865de2edf563973f45371
Aleksei Naumyshev <naumyshev@yandex.ru>
2022-10-29 14:43:46 +0300

    #21 completed: refactored Dialogs.tsx, Dialogs.module.css

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,39 @@ import s from './Dialogs.module.css'
 
 export const Dialogs = () => {
     return (
-        <div>
-            <h1>Dialogs</h1>
+        <div className={s.dialogs}>
+            <div className={s.dialogItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Dimych
+                </div>
+                <div className={s.dialog}>
+                    Sveta
+                </div>
+                <div className={s.dialog}>
+                    Andrey
+                </div>
+                <div className={s.dialog}>
+                    Sasha
+                </div>
+                <div className={s.dialog}>
+                    Viktor
+                </div>
+                <div className={s.dialog}>
+                    Valera
+                </div>
+
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>
+                    Hi
+                </div>
+                <div className={s.message}>
+                    How is your it-kamasutra?
+                </div>
+                <div className={s.message}>
+                    Yo!
+                </div>
+            </div>
         </div>
     );
 };
