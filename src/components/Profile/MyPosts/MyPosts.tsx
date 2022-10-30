dbc39c80d01a52eefb8dc17962dff684733bd310
Aleksei Naumyshev <naumyshev@yandex.ru>
2022-10-30 11:34:55 +0300

    #24 completed: BLL out UI

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -3,6 +3,12 @@ import s from "./MyPosts.module.css";
 import {Post} from "./Post/Post";
 
 export const MyPosts = () => {
+
+    const postsDada = [
+        {id: 1, message: 'Hi! How are you?', likesCount: 12},
+        {id: 2, message: 'My first post', likesCount: 1}
+    ]
+
     return (
         <div className={s.postsBlock}>
             <h3>My posts</h3>
@@ -14,10 +20,8 @@ export const MyPosts = () => {
             </div>
 
             <div>
-                <Post message='Hi! How are you?' likesCount={21} />
-                <Post message='My first post' likesCount={3}/>
-
-
+                <Post message={postsDada[0].message} likesCount={postsDada[0].likesCount} />
+                <Post message={postsDada[1].message} likesCount={postsDada[1].likesCount} />
             </div>
         </div>
     );
