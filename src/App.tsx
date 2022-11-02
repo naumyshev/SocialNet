8eefe4607ad57573d0664651dfa67778a4969a33
Aleksei Naumyshev <naumyshev@yandex.ru>
2022-11-02 17:28:24 +0300

    #30 refactored BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import {Header} from "./components/Header/Header";
 import {Navbar} from "./components/Navbar/Navbar";
 import {Profile} from "./components/Profile/Profile";
 import {Dialogs} from "./components/Dialogs/Dialogs";
-import {BrowserRouter, Route} from "react-router-dom";
+import {Route} from "react-router-dom";
 import {News} from "./components/News/News";
 import {Music} from "./components/Music/Music";
 import {Settings} from "./components/Settings/Settings";
@@ -17,22 +17,17 @@ type AppPropsType = {
 
 function App(props: AppPropsType) {
     return (
-        <BrowserRouter>
             <div className="app-wrapper">
                 <Header/>
                 <Navbar/>
                 <div className="app-wrapper-content">
-
                     <Route path='/dialogs' render={()=><Dialogs state={props.state.dialogsPage}/>}/>
                     <Route path='/profile' render={()=><Profile state={props.state.profilePage}/>}/>
                     <Route path='/news' render={()=><News />}/>
                     <Route path='/music' render={()=><Music />}/>
                     <Route path='/settings' render={()=><Settings />}/>
-
                 </div>
-
             </div>
-        </BrowserRouter>
     );
 }
 
