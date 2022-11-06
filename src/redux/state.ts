96bbbbb6076ab11776fcaf094424c461cec07ea2
Aleksei Naumyshev <naumyshev@yandex.ru>
2022-11-06 20:45:04 +0300

    #37 added store

--- src/redux/state.ts
+++ src/redux/state.ts
@@ -2,10 +2,6 @@ import {PostType} from "../components/Profile/MyPosts/Post/Post";
 import {DialogItemPropsType} from "../components/Dialogs/DialogItem/DialogItem";
 import {MessagePropsType} from "../components/Dialogs/Message/Message";
 
-let rerenderEntireTree = (state: RootStateType) => {
-
-}
-
 export type ProfilePageType = {
     posts: Array<PostType>
     newPostText: string
@@ -21,47 +17,50 @@ export type RootStateType = {
     dialogsPage: DialogPageType
 }
 
-export const state: RootStateType = {
-    profilePage: {
-        posts: [
-            {id: 1, message: 'Hi! How are you?', likesCount: 12},
-            {id: 2, message: 'My first post', likesCount: 1},
-            {id: 3, message: 'Yo!', likesCount: 1},
-            {id: 4, message: 'Yo! Yo!', likesCount: 1}
-        ],
-        newPostText: ''
+export const store = {
+    _state: {
+        profilePage: {
+            posts: [
+                {id: 1, message: 'Hi! How are you?', likesCount: 12},
+                {id: 2, message: 'My first post', likesCount: 1},
+                {id: 3, message: 'Yo!', likesCount: 1},
+                {id: 4, message: 'Yo! Yo!', likesCount: 1}
+            ],
+            newPostText: ''
+        },
+        dialogsPage: {
+            dialogs: [
+                {id: 1, name: 'Dimych'},
+                {id: 2, name: 'Andrey'},
+                {id: 3, name: 'Sveta'},
+                {id: 4, name: 'Sasha'},
+                {id: 5, name: 'Viktor'},
+                {id: 6, name: 'Valera'}
+            ],
+            messages: [
+                {id: 1, message: 'Hi'},
+                {id: 2, message: 'How is your it-kamasutra?'},
+                {id: 3, message: 'Yo!'},
+                {id: 4, message: 'Yo!'},
+                {id: 5, message: 'Yo!'},
+            ]
+        }
     },
-    dialogsPage: {
-        dialogs: [
-            {id: 1, name: 'Dimych'},
-            {id: 2, name: 'Andrey'},
-            {id: 3, name: 'Sveta'},
-            {id: 4, name: 'Sasha'},
-            {id: 5, name: 'Viktor'},
-            {id: 6, name: 'Valera'}
-        ],
-        messages: [
-            {id: 1, message: 'Hi'},
-            {id: 2, message: 'How is your it-kamasutra?'},
-            {id: 3, message: 'Yo!'},
-            {id: 4, message: 'Yo!'},
-            {id: 5, message: 'Yo!'},
-        ]
+    rerenderEntireTree (state: RootStateType)  {
+
+    },
+    addPost () {
+        const newPost: PostType = {id: 5, message: this._state.profilePage.newPostText, likesCount: 0}
+        this._state.profilePage.posts.push(newPost)
+        this._state.profilePage.newPostText = ''
+        this.rerenderEntireTree(this._state)
+    },
+    updateNewPostText (newText: string) {
+        this._state.profilePage.newPostText = newText
+        this.rerenderEntireTree(this._state)
+    },
+    subscribe (observer: (state: RootStateType) => void)  {
+        this.rerenderEntireTree = observer
     }
 }
 
-export const addPost = () => {
-    const newPost: PostType = {id: 5, message: state.profilePage.newPostText, likesCount: 0}
-    state.profilePage.posts.push(newPost)
-    state.profilePage.newPostText = ''
-    rerenderEntireTree(state)
-}
-
-export const updateNewPostText = (newText: string) => {
-    state.profilePage.newPostText = newText
-    rerenderEntireTree(state)
-}
-
-export const subscribe = (observer: (state: RootStateType) => void) => {
-    rerenderEntireTree = observer
-}
